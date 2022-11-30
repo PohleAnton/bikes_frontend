1eae82589ee0f9cae33509334e41b0635e37afc6
PohleAnton <s0582388@htw-berlin.de>
2022-11-30 14:26:13 +0100

    test

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>It puts the lotion on its skin or else it gets the hose again</h1>
   </div>
 </template>
---
+++ src/views/BikesView.vue
@@ -0,0 +1,13 @@
+<template>
+<h1>Bikes</h1>
+</template>
+
+<script>
+export default {
+  name: 'BikesView'
+}
+</script>
+
+<style scoped>
+
+</style>
