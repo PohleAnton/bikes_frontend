d1e6ecd3ca9092ae694f1d0cae7de40553a8312b
Marvin Petsch <marvinpetsch@mailbox.org>
2023-01-03 12:41:11 +0100

    input und select felder für fahrraderstellung

--- src/views/NeuesFahrradView.vue
+++ src/views/NeuesFahrradView.vue
@@ -3,7 +3,60 @@
     Neues Angebot erstellen
   </h1>
   <div>
-    <input v-model = "nameField" placeholder = "Name" type = "text" ref="nameInput">
+    <p> Kategorie:</p>
+    <select v-model = "kategorie">
+      <option disabled value = "">Bitte wähle eins aus</option>
+      <option>SPORTFAHRRAD</option>
+      <option>EBIKE</option>
+      <option>MOUNTAINBIKE</option>
+      <option>CITYBIKE</option>
+      <option>KINDERFAHRRAD</option>
+      <option>SONSTIGE</option>
+    </select>
+    <p></p>
+  </div>
+  <div>
+    <p> Abnutzungsgrad:</p>
+    <select v-model = "abnutzungsgrad">
+      <option disabled value = "">Bitte wähle eins aus</option>
+      <option>NEU</option>
+      <option>GERINGE ABNUTZUNG</option>
+      <option>MITTLERE ABNUTZUNG</option>
+      <option>STARKE ABNUTZUNG</option>
+    </select>
+    <p></p>
+  </div>
+  <div>
+    <p> Farbe:</p>
+    <select v-model = "farbe">
+      <option disabled value = "">Bitte wähle eins aus</option>
+      <option>SCHWARZ</option>
+      <option>GRAU</option>
+      <option>WEISS</option>
+      <option>ROT</option>
+      <option>BLAU</option>
+      <option>GRUEN</option>
+      <option>SONSTIGE</option>
+      <option>MIXED</option>
+    </select>
+    <p></p>
+  </div>
+  <p>Weiteres:  </p>
+  <div>
+    <input v-model = "preis" placeholder = "preis" type = "text" ref="nameInput">
+    <p></p>
+  </div>
+  <div>
+    <input v-model = "bildUrl" placeholder = "bildUrl" type = "text" ref="nameInput">
+    <p></p>
+  </div>
+  <div>
+    <input v-model = "kurzbeschreibung" placeholder = "kurzbeschreibung" type = "text" ref="nameInput">
+    <p></p>
+  </div>
+  <div>
+    <input v-model = "langbeschreibung" placeholder = "langbeschreibung" type = "text" ref="nameInput">
+    <p></p>
   </div>
 </template>
 
