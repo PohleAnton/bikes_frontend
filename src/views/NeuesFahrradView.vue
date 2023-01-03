f4105c79e7f04c67dd33269d9862372f1d90326f
Marvin Petsch <marvinpetsch@mailbox.org>
2023-01-03 12:20:03 +0100

    input-felder-testen

--- src/views/NeuesFahrradView.vue
+++ src/views/NeuesFahrradView.vue
@@ -2,11 +2,19 @@
   <h1>
     Neues Angebot erstellen
   </h1>
+  <div>
+    <input v-model = "nameField" placeholder = "Name" type = "text" ref="nameInput">
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'NeuesFahrradView'
+  name: 'NeuesFahrradView',
+  data() {
+    return {
+      nameField:''
+    }
+  }
 }
 </script>
 
